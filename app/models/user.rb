5e87589dc4d22097f0d17bd329ae04071e35c88c
Anastasia <z.anastasia11@gmail.com>
2023-08-03 14:29:52 -0700

    atyle signup form and add auth to order controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   #gives our User model authentication methods via bcrypt
   has_secure_password
+  validates :email, uniqueness: { message: "this email already registered, please enter anothor one" }
 end
