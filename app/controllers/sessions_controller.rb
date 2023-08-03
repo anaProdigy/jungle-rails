f71cdc2bd291e015c3d4c65556c523aff672b95f
Anastasia <z.anastasia11@gmail.com>
2023-08-03 12:32:24 -0700

    Create a sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+#This is where we create (aka login) and destroy (aka logout) sessions
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  #gives our User model authentication methods via bcrypt
   has_secure_password
 end
