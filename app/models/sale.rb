84172dfcca22d240ef884464c00bde0881de97b2
Anastasia <z.anastasia11@gmail.com>
2023-08-03 20:33:49 -0700

    add alert about a sale on mane page

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,8 @@
 class Sale < ApplicationRecord
+#Acrive Record scope
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
 
   def finished?
     ends_on < Date.current
