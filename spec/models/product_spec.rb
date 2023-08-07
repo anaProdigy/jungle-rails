d37a60d2990a72eadb55171ba330b4a405bc0618
Anastasia <z.anastasia11@gmail.com>
2023-08-07 12:27:20 -0700

    add tests to validate all fieds are filles, name and price are missing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    before(:each) do
+    @category = Category.create(name: "Test Category")
+    end
+
+    it "saves successfully with all four fields set" do
+    # create a product with all fields set
+      product = Product.create(
+        name: 'Test Product',
+        price: 10.99,
+        quantity: 5,
+        category: @category
+      )
+      
+      expect(product).to be_valid
+    end
+
+    it "requires name to be set" do
+    # create a product with name missing
+      product = Product.create(
+        price: 10.99,
+        quantity: 5,
+        category: @category
+      )
+
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "requires price to be set" do
+    # create a product with price missing
+      product = Product.create(
+        name: 'Test Product',
+        quantity: 5,
+        category: @category
+      )
+
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Price can't be blank")
+    end
+
+  end
 end
