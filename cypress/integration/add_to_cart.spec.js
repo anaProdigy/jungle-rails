2c2d8ad2de22abfcab29a0078e47c4f44a95b6b3
Anastasia <z.anastasia11@gmail.com>
2023-08-08 20:46:52 -0700

    add changes to aadd to cart test

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -11,7 +11,7 @@ it("Click the 'Add to Cart' button for a product on the home page increases cart
   cy.get(".btn")
     .first()
     .click({ force: true });
-  // Assert that the URL has changed to the expected product detail page
+ 
   // Assert that the cart count has increased by one
   cy.contains("My Cart (1)")
 
