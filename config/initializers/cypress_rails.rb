b387086904f5a9abda1b6fbe76bcdbe44fb70bb7
Anastasia <z.anastasia11@gmail.com>
2023-11-28 09:58:13 -0800

    fix cleaner

--- config/initializers/cypress_rails.rb
+++ config/initializers/cypress_rails.rb
@@ -1,53 +1,50 @@
-require 'database_cleaner/active_record'
-DatabaseCleaner.strategy = :truncation
+if Rails.env.test?
+  require 'database_cleaner/active_record'
+  DatabaseCleaner.strategy = :truncation
 
-return unless Rails.env.test?
+  CypressRails.hooks.before_server_start do
+    # Create a test user
+    User.create!(
+      first_name: "Bob",
+      last_name: "Great",
+      email: "bob@bob.com",
+      password: "password",
+      password_confirmation: "password"
+    )
 
-CypressRails.hooks.before_server_start do
+    # Called once, before either the transaction or the server is started
+    cat1 = Category.find_or_create_by! name: 'Evergreens'
 
-  # Create a test user
-  User.create!(
-    first_name: "Bob",
-    last_name: "Great",
-    email: "bob@bob.com",
-    password: "password",
-    password_confirmation: "password"
-  )
-  # Called once, before either the transaction or the server is started
-  cat1 = Category.find_or_create_by! name: 'Evergreens'
+    cat1.products.create!({
+      name:  'Giant Tea',
+      description: "The Giant Tea is an uncommon, medium-sized plant and can be found only in some tundras. It blooms twice a year, for 3 weeks.",
+      image: open_asset('plante_1.jpg'),
+      quantity: 0,
+      price: 64.99
+    })
 
-  cat1.products.create!({
-    name:  'Giant Tea',
-    description: "The Giant Tea is an uncommon, medium-sized plant and can be found only in some tundras. It blooms twice a year, for 3 weeks.",
-    image: open_asset('plante_1.jpg'),
-    quantity: 0,
-    price: 64.99
-  })
+    cat1.products.create!({
+      name:  'Scented Blade',
+      description: "The Scented Blade is an extremely rare, tall plant and can be found mostly in savannas. It blooms once a year, for 2 weeks.",
+      image: open_asset('plante_2.jpg'),
+      quantity: 18,
+      price: 24.99
+    })
+  end
 
-  cat1.products.create!({
-    name:  'Scented Blade',
-    description: "
-    The Scented Blade is an extremely rare, tall plant and can be found mostly in savannas. It blooms once a year, for 2 weeks.",
-    image: open_asset('plante_2.jpg'),
-    quantity: 18,
-    price: 24.99
-  })
+  CypressRails.hooks.after_transaction_start do
+    # Called after the transaction is started (at launch and after each reset)
+  end
 
+  CypressRails.hooks.after_state_reset do
+    # Triggered after `/cypress_rails_reset_state` is called
+  end
+
+  CypressRails.hooks.before_server_stop do
+    # Called once, at_exit
+    DatabaseCleaner.clean
+  end
 end
-
-CypressRails.hooks.after_transaction_start do
-  # Called after the transaction is started (at launch and after each reset)
-end
-
-CypressRails.hooks.after_state_reset do
-  # Triggered after `/cypress_rails_reset_state` is called
-end
-
-CypressRails.hooks.before_server_stop do
-  # Called once, at_exit
-  DatabaseCleaner.clean
-end
-
 
 
 
